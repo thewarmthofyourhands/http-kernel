2097b710ac6b73b653460a9408de95cce7781d40
thewarmthofyourhands <rostislavpanteleychuk@gmail.com>
2022-11-25 02:22:07 +0400

    fix

--- src/EventListeners/ExceptionListener.php
+++ src/EventListeners/ExceptionListener.php
@@ -4,27 +4,28 @@ declare(strict_types=1);
 
 namespace Eva\HttpKernel\EventListeners;
 
-use Eva\Env\Env;
 use Eva\Http\Message\Response;
 use Eva\HttpKernel\Events\ExceptionEvent;
 use Eva\HttpKernel\Exceptions\HttpException;
 
 class ExceptionListener
 {
-    public function __construct(protected Env $env) {}
+    public function __construct(protected string $dev) {}
 
     public function __invoke(ExceptionEvent $exceptionEvent): ExceptionEvent
     {
         $throwable = $exceptionEvent->getThrowable();
 
-        $message = sprintf(
-            'error code: %s, message: %s, file: %s, line: %s, trace: %s',
-            $throwable->getCode(),
-            $throwable->getMessage(),
-            $throwable->getFile(),
-            $throwable->getLine(),
-            $throwable->getTraceAsString(),
-        );
+        if ($this->dev === 'dev') {
+            $message = sprintf(
+                'error code: %s, message: %s, file: %s, line: %s, trace: %s',
+                $throwable->getCode(),
+                $throwable->getMessage(),
+                $throwable->getFile(),
+                $throwable->getLine(),
+                $throwable->getTraceAsString(),
+            );
+        }
 
         if ($throwable instanceof HttpException) {
             $response = new Response($throwable->getResponseStatusCode(), [], $message);
